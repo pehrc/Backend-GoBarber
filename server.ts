d97ab129fca8ab2fe0416701b16d32b582096eca
unknown <correia.pedro@outlook.com>
2020-11-25 15:57:07 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+import './database';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
